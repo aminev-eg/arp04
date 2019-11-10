3f16dd6f44ef31a73757b70ab2ed29274c73fd6d
AmiEG <anton29141@gmail.com>
2019-11-10 12:58:26 +0300

    (1) to br1

--- app/src/main/java/com/example/arp04/MainActivity.kt
+++ app/src/main/java/com/example/arp04/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.arp04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentConteiner, FragmentFirst())
+            .commit()
+
+        buttonFragment1.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentConteiner, FragmentFirst())
+                .commit()
+        }
+
+        buttonFragment2.setOnClickListener{     //some comment to git
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentConteiner, FragmentSecond())
+                .commit()
+        }
     }
 }
