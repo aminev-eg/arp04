62b8df5c678240d7ee60703d8af09182bdc78988
AmiEG <anton29141@gmail.com>
2019-11-10 12:59:56 +0300

    (m) (0-3) somre comment

--- app/src/main/java/com/example/arp04/MainActivity.kt
+++ app/src/main/java/com/example/arp04/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.arp04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //another comment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
